81dd02a3563d20cbd82205cfe843c5a0ca7bc5e1
hayamaster <dnfxmfkdbzos@naver.com>
2022-03-02 16:53:06 +0900

    first commit

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const background = "pastel color.png";
+
+const image = document.createElement("img");
+image.src = `photo/${background}`;
+image.classList.add("image");
+
+document.body.appendChild(image);
